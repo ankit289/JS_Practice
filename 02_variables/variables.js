825c712870676b353d192a265522e6cfe4596165
Ankit <43002190+ankit289@users.noreply.github.com>
2024-04-23 09:21:26 +0000

    update variables

--- 02_variables/variables.js
+++ 02_variables/variables.js
@@ -1,9 +1,22 @@
 // automatically creation of variables
 
-userId='45'
+userId=45
 console.log(userId);
 
 // use var keyword
-// modern day javascript d
+// modern day javascript does not uses var 
 var email='abc@gmail.com'
 console.log(email);
+
+//use let keyword
+
+let phoneNumber=888998
+console.log(phoneNumber);
+
+//const kewyword for constants
+
+const id=122
+console.log(id);
+
+//console.table presents all things in one table
+console.table([userId,email,phoneNumber,id])
